c93e4806178d58b9b9e7db3e3645c5293f45c40c
krishna <krishgithub10@gmail.com>
2023-01-01 18:14:20 +0530

    about section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./About.css";
+import Hello from "./imgs/about.png";
+import { BsCheck2All } from "react-icons/bs";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="container">
+        <img src={Hello} alt="hello" />
+        <div className="col-2">
+          <h2>
+            <span className="gray">About </span>
+            <span className="purple"> Me</span>
+          </h2>
+          <p>Passionate person who believes in universe</p>
+          <ul>
+            <li>
+              <BsCheck2All
+                className="double-tick"
+                fill="#b161e7"
+                size="1.2em"
+              />
+              I am a Self taught programmer, intrestred in various technologies,
+              currently pursuing B.Sc. Physics.
+            </li>
+            <li>
+              <BsCheck2All
+                className="double-tick"
+                fill="#b161e7"
+                size="1.2em"
+              />
+              Freelance Teacher - Programming
+            </li>
+            <li>
+              <BsCheck2All
+                className="double-tick"
+                fill="#b161e7"
+                size="1.2em"
+              />
+              Gaming Enthusiast
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
